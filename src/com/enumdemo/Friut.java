00c7fc286a880de57018ac5e9fe4aca02cd80436
cao x <1044434566@qq.com>
2019-02-18 15:34:58 +0800

    枚举

--- src/com/enumdemo/Friut.java
+++ src/com/enumdemo/Friut.java
@@ -17,9 +17,25 @@ public enum Friut {
     public static void main(String[] args) {
         Friut[] friuts = Friut.values();
         for (Friut friut : friuts) {
-            System.out.println(friut.code+friut.name()+friut.ordinal());
+            System.out.println("friut.ordinal():"+friut.ordinal()
+                    +",friut.code:"+friut.code
+                    +",friut.name():"+friut.name());
+            /**
+             * friut.ordinal():0,friut.code:1,friut.name():APPLE
+             * friut.ordinal():1,friut.code:2,friut.name():ORANGE
+             * friut.ordinal():2,friut.code:3,friut.name():BANANA
+             */
+
         }
-        System.out.println(Friut.APPLE);
+        System.out.println(Friut.APPLE);//APPLE
+        System.out.println(Friut.APPLE.code);//1
+        /**
+         * valueOf()方法会把一个String类型的名称转变为枚举项,也就是枚举项中查找出字面值与该参数相等的枚举项
+         */
+//        System.out.println(Friut.valueOf("apple1"));//No enum constant com.enumdemo.Friut.apple1
+        System.out.println(Friut.valueOf("APPLE"));//APPLE
+        System.out.println(Friut.valueOf("APPLE").code);//1   和前面一样，那valueof()感觉没什么用
+
     }
 
 }
